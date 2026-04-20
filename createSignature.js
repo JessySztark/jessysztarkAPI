51af2b33873982e38fb601e5e605216ae6f88a63
Jessy <jessysztark@gmail.com>
2026-04-21 01:17:08 +0200

    [1.4.1] Adding Signature system for La Poudriere de Porks

--- createSignature.js
+++ createSignature.js
@@ -1,8 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 
-function createSignature(fileBytes, employee) {
-    fs.writeFileSync(path.join(process.cwd(), `signatures-poudriere/${employee}.png`), Buffer.from(fileBytes, 'base64'));
+async function createSignature(fileBytes, employee) {
+    await fs.writeFileSync(path.join(process.cwd(), `signatures-poudriere/${employee}.png`), Buffer.from(fileBytes, 'base64'));
     return `Signature de ${employee} enregistrée avec succès!`;
 }
 
