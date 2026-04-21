f6ff9a26a9c4fcf3484d7fdf7d6ffe09cc48f72c
Jessy <jessysztark@gmail.com>
2026-04-21 11:16:10 +0200

    [1.4.2] Changing create signature name write

--- createSignature.js
+++ createSignature.js
@@ -2,7 +2,9 @@ import fs from 'fs';
 import path from 'path';
 
 async function createSignature(fileBytes, employee) {
-    await fs.writeFileSync(path.join(process.cwd(), `signatures-poudriere/${employee}.png`), Buffer.from(fileBytes, 'base64'));
+    var name = employee.split(" ")[0].toLowerCase() + "-" + employee.split(" ")[1].toLowerCase();
+    const signaturesDir = path.join(process.cwd(), "signatures-poudriere");
+    await fs.writeFileSync(path.join(signaturesDir, `${name}.png`), Buffer.from(fileBytes, 'base64'));
     return `Signature de ${employee} enregistrée avec succès!`;
 }
 
